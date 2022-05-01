33778a61fdc4424abe953fb2922ac51360b59521
hykimyong <hykimyong@naver.com>
2022-05-01 16:29:46 +0900

    비트코인 api 가져와서 정보 뿌려주기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import {useState, useEffect} from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if(toDo === ""){
-            return;
-        }
-
-        setToDos((currrentArray) => [toDo, ...currrentArray]);
-        /*
-        const food = [1,2,3,4];
-        [6,food]
-        결과를 보면 이상하게 들어감 4 뒤에 6을 추가하고싶을때는
-        [6, ...food]
-        이렇게 쓰면 [1,2,3,4,6]이렇게 들어감
-        */
-        setToDo("");
-    }
-    console.log(toDos);
-    console.log(toDos.map((item, index)=> <li key={index}>{item}</li>));
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    useEffect(()=>{
+        fetch("https://api.coinpaprika.com/v1/tickers")
+        .then((response)=>response.json())
+        .then((json)=>{
+            setCoins(json);
+            setLoading(false);
+        });
+    }, []);
     return (
       <div>
-        <h1>My To Dos ({toDos.length})</h1>
-        <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
-        <button>Add to Do</button>
-        </form>
-        <hr/>
-        <ul>
-        {toDos.map((item, index)=> <li key={index}>{item}</li>)}
-        </ul>
+        <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+        {loading ? <strong>Loading...</strong> : (<select>
+             {coins.map((coin)=> <option>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}</option>)}
+         </select> )}
       </div>
     );
 }
