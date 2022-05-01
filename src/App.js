200b8406784836a230d8700664545c1abc6299fa
hykimyong <hykimyong@naver.com>
2022-05-01 15:59:17 +0900

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import {useState, useEffect} from "react";
 
-function Hello(){
-
-    useEffect(()=>{
-            console.log('hi');
-            return ()=> console.log('bye');
-        },[]);
-//    useEffect(function(){
-//        console.log('hi');
-//        return function(){
-//            console.log('bye');
-//        }
-//    },[]);
-    return <h1>Hello</h1>
-}
-
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev)=> !prev);
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if(toDo === ""){
+            return;
+        }
+
+        setToDos((currrentArray) => [toDo, ...currrentArray]);
+        /*
+        const food = [1,2,3,4];
+        [6,food]
+        결과를 보면 이상하게 들어감 4 뒤에 6을 추가하고싶을때는
+        [6, ...food]
+        이렇게 쓰면 [1,2,3,4,6]이렇게 들어감
+        */
+        setToDo("");
+    }
+    console.log(toDos);
     return (
       <div>
-        {showing ? <Hello/> : null}
-        <button onClick = {onClick}>{showing ? "Hide" : "Show"}</button>
+        <h1>My To Dos ({toDos.length})</h1>
+        <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
+        <button>Add to Do</button>
+        </form>
       </div>
     );
 }
